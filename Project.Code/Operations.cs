e512b8ee3aa921fef4b276f960b3a85d14c8d1fd
Smart\Amanda <adraganjac93@gmail.com>
2016-10-27 22:47:57 +0200

    prva verzija

---
+++ Project.Code/Operations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Code
+{
+    public static class Operations
+        {
+            public const string Enlist = "ENLIST";
+            public const string Display = "DISPLAY";
+        }
+}
---
+++ Project.Code/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Code
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+    }
+}
