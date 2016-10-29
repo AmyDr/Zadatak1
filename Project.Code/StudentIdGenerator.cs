becca958655c6a16db75c27f9de50986eb33e30b
Smart\Amanda <adraganjac93@gmail.com>
2016-10-29 03:37:00 +0200

    1.1

--- Project.Code/StudentIdGenerator.cs
+++ Project.Code/StudentIdGenerator.cs
@@ -7,5 +7,20 @@ namespace Project.Code
 {
     public class StudentIdGenerator
     {
+            private static StudentIdGenerator instance;
+            private int nextID;
+            private StudentIdGenerator() { nextID = 1; }
+            public static StudentIdGenerator getGenerator()
+            {
+                if (instance == null)
+                {
+                    instance = new StudentIdGenerator();
+                }
+                return instance;
+            }
+            public int nextId()
+            {
+                return nextID++;
+            }
     }
 }
