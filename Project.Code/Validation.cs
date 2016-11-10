ea699f07fcddb925c87c6d6f396f2613afffbfa6
Smart\Amanda <adraganjac93@gmail.com>
2016-11-11 00:10:36 +0100

    version 1.3

--- Project.Code/Validation.cs
+++ Project.Code/Validation.cs
@@ -8,12 +8,28 @@ namespace Project.Code
     //validation class with methods to validate string (null or empty), operation name and GPA's appropriate data type
     public class Validation
     {
-        public static string Validation(string s)
+        public static bool ValidateString(string s)
         {
-            if (String.IsNullOrEmpty(s))
-                return "is null or empty";
+            if (string.IsNullOrEmpty(s))
+                return true;
             else
-            { }
+                return false;
+        }
+
+        public static bool ValidateGpa(string gpa)
+        {
+            bool result;
+            double number;
+            result = double.TryParse(gpa, out number);
+            return result;
+        }
+
+        public static bool ValidateOperation(string operation)
+        {
+            if (operation != Operations.Enlist && operation != Operations.Display)
+                return true;
+            else
+                return false;
         }
     }
 }
